9b8b037a4d555c9644fd8d5152260733bd672277
吴博聪 <wubocong123@163.com>
2015-07-27 20:34:21 +0800

    v0.4.2

--- public/js/photo.js
+++ public/js/photo.js
@@ -1,117 +1,120 @@
-var margin = 5,
-    li,
-    li_W = (screen.width * 0.8 - 30) / 4,
-    photo,
-    i = 1,
-    order = 0;
+jQuery(function() {
+    var margin = 5,
+        li,
+        li_W = (screen.width * 0.8 - 30) / 4,
+        photo,
+        i = 1,
+        order = 0,
+        scrollTop = 0,
+        image = $('#large'),
+        other = $('#other');
 
-function getData(imgUrl) {
-    $.ajax({
-        type: 'GET',
-        url: imgUrl,
-        dataType: 'json',
-        cache: false,
-        success: function(data) {
-            photo = data.photo;
-            $(window).bind('scroll', function() {
-                if ($(document).scrollTop() + $(window).height() > $(document).height() - 10 && i == 1) {
-                    i = 0;
-                }
+    function getData(imgUrl) {
+        $.ajax({
+            type: 'GET',
+            url: imgUrl,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                photo = data.photo;
+                $(window).bind('scroll', function() {
+                    if ($(document).scrollTop() + $(window).height() > $(document).height() - 10 && i == 1) {
+                        i = 0;
+                    }
+                });
+                getMore();
+            }
+        });
+    }
+
+    function refresh() {
+        other.css('height', (screen.availHeight - 50 > 0) ? screen.availHeight - 50 : 0);
+        li = $('#box li');
+        var h = [];
+        var n = screen.availWidth / (li_W + 10) | 0;
+        for (var i = 0; i < li.length; i++) {
+            var li_H = li.eq(i).find('img')[0].height;
+            if (i < n) {
+                h[i] = li_H;
+                li.eq(i).css('top', 0);
+                li.eq(i).css('left', i * (li_W + 10));
+            } else {
+                min_H = Math.min.apply(null, h);
+                minKey = getarraykey(h, min_H);
+                h[minKey] += li_H + margin;
+                li.eq(i).css('top', min_H + margin);
+                li.eq(i).css('left', minKey * (li_W + 10));
+            }
+            li.eq(i).css('opacity', 1);
+        }
+    }
+
+    function getarraykey(s, v) {
+        for (var k in s) {
+            if (s[k] == v) {
+                return k;
+            }
+        }
+    }
+
+    function getMore() {
+        for (var j = 0; j < 50 && order < photo.length; j++, order++) {
+            $('#loading').show();
+            var url = photo[order].url;
+            var html = '<li><a href="javascript:void(0)"><img src=' + url + ' ></a></li>';
+            $('#box').append(html);
+            $('#box img').css({
+                'width': li_W
             });
-            getMore();
+            $('#loading').hide();
         }
-    });
-}
-
-function refresh() {
-    $('#other').css('height', (screen.availHeight - 50 > 0) ? screen.availHeight - 50 : 0);
-    li = $('#box li');
-    var h = [];
-    var n = screen.availWidth / (li_W + 10) | 0;
-    for (var i = 0; i < li.length; i++) {
-        var li_H = li.eq(i).find('img')[0].height;
-        if (i < n) {
-            h[i] = li_H;
-            li.eq(i).css('top', 0);
-            li.eq(i).css('left', i * (li_W + 10));
-        } else {
-            min_H = Math.min.apply(null, h);
-            minKey = getarraykey(h, min_H);
-            h[minKey] += li_H + margin;
-            li.eq(i).css('top', min_H + margin);
-            li.eq(i).css('left', minKey * (li_W + 10));
-        }
-        li.eq(i).css('opacity', 1);
+        zoom();
+        refresh();
+        i = 1;
     }
-}
 
-function getarraykey(s, v) {
-    for (var k in s) {
-        if (s[k] == v) {
-            return k;
-        }
-    }
-}
-
-function getMore() {
-    for (var j = 0; j < 50 && order < photo.length; j++, order++) {
-        $('#loading').show();
-        var url = photo[order].url;
-        var html = '<li><a href="#"><img src=' + url + ' ></a></li>';
-        $('#box').append(html);
-        $('#box img').css({
-            width: li_W
-        });
-        $('#loading').hide();
-    }
-    $('#box img').click(function() {
-        var image = $(this),
-            other = $('#other'),
-            parent = image.parent().parent();
-        var left = parent.css('left'),
-            top = parent.css('top');
-        image.css({
-            'width': screen.availWidth * 0.8,
-        });
-        parent.css({
-            'left': 0,
-            'top': 0,
-            'margin': 'auto',
-            'z-index': 2
-        });
-        other.css({
-            'z-index': 1,
-            'opacity': 0.5
-        });
-        other.click(function() {
+    function zoom() {
+        $('#box img').click(function() {
+            image[0].src = $(this)[0].src;
+            if(image[0].width<image[0].height)
+            {
+                image.css({'height':screen.availHeight,'width':'auto'});
+            }
             image.css({
-                'width': li_W,
-            });
-            parent.css({
-                'left': left,
-                'top': top,
-                'margin': 0,
-                'z-index': 0
-            });
-            $(this).css({
-                'z-index': -1,
-                'opacity': 0
-            });
+                'top': scrollTop + 50,
+                'z-index': 2
+            }).
+            css('opacity', 1);
+            other.css({
+                'top': scrollTop + 50,
+                'z-index': 1
+            }).css('opacity', 0.5);
+        });
+    }
+    $('#nav li').click(function() {
+        order = 0;
+        var option = this.id;
+        var baseUrl = 'public/data/';
+        $('#box').empty();
+        $('#nav li').removeClass('active');
+        $(this).addClass('active');
+        getData(baseUrl + option + '.js');
+    });
+    
+    other.click(function() {
+        image.css('opacity', 0).css('z-index', -1);
+        $(this).css({
+            'opacity': 0
+        }).css({
+            'z-index': -1
         });
     });
-    refresh();
-    i = 1;
-}
-$('#nav li').click(function() {
-    order = 0;
-    var option = this.id;
-    var baseUrl = 'public/data/';
-    $('#box').empty();
-    $('#nav li').removeClass('active');
-    $(this).addClass('active');
-    getData(baseUrl + option + '.js');
+    window.onresize = function() {
+        refresh();
+    };
+    window.onscroll = function() {
+        scrollTop = document.body.scrollTop;
+        $('#nav').css('top', scrollTop);
+    };
+    getData('public/data/pal.js');
 });
-getData('public/data/pal.js');
-window.onresize = function() {
-    refresh();
-};
